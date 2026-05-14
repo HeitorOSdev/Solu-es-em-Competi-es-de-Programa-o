6165a9b1b0f41582ab9cabb267c599e956cd1078
HeitorOtaciano <164053220+HeitorOtaciano@users.noreply.github.com>
2026-05-13 23:44:07 -0300

    Mais uma resolvida, agora estou no meio do caminho com a B do contest ABC455

---
+++ Resoluções/Unidade I/Questões avulsas/Solo/AtCoder - Contest ABC 455/B - incompleta.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0); cin.tie(0);
+    
+    int H,W, output = 0;
+    cin >> H >> W;
+    
+    vector<string> v(H);
+    
+    
+    for(int i = 0; i < H; i++){
+        string s;
+        getline(cin, s);
+        v.push_back(s);
+    }
+    
+    
+    
+    for(int i = 0; i<W; i++){
+        for(int j=0; j<H; j++){
+            
+            if(v[i])
+            
+            if(v[]%2 == 0)
+            
+        }
+    }
+    
+    
+    
+    
+    return 0;
+}

8333d01c531e32f57d4b8ecd4b80d79667a290a0
HeitorOtaciano <164053220+HeitorOtaciano@users.noreply.github.com>
2026-05-19 11:43:54 -0300

    +1 questão fácil

---
+++ Resoluções/Unidade I/Questões avulsas/Solo/AtCoder - Contest ABC 446/A - Resolvida.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);
+    
+    string S;
+    
+    cin >> S;
+    
+    S[0] = tolower(S[0]);
+    
+    cout << "Of" << S << "\n";
+
+    return 0;
+}

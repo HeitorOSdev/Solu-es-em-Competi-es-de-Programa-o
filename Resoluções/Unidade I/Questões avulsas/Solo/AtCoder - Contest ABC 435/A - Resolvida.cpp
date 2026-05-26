5f0b8423c5d5d89501d433cb6b08499f5806acf2
HeitorOtaciano <164053220+HeitorOtaciano@users.noreply.github.com>
2026-05-26 12:12:04 -0300

    +1 feita

---
+++ Resoluções/Unidade I/Questões avulsas/Solo/AtCoder - Contest ABC 435/A - Resolvida.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // ios::sync_with_stdio(0); cin.tie(0);
+    
+    int N;
+    cin >> N;
+    
+    int result = (N+1)*(N/2);
+    if(N%2 != 0) result += (N+1)/2;
+    
+    cout << result << "\n";
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return 0;
+}
